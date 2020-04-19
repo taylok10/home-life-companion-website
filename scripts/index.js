264718413026a80729b3a9691de1b7ee36dca8cc
Kelly Taylor <taylok10@aston.ac.uk>
2020-04-19 16:53:25 +0100

    New website look

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+function signup(email){
+    alert(`Thank you for signing up! Due to GDPR considerations we have not implemented this functionality as part of our group 
+    project. If we had, it would have stored the emails in a database and then updated the users each time there was a blog post 
+    update`);
+}
