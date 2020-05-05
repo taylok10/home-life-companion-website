bb7c8f67f25f43de12104f112ebd9cb0695ba61b
Kelly Taylor <taylok10@aston.ac.uk>
2020-05-05 21:20:54 +0100

    Blog navigation

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,3 +1,5 @@
+var latestNavMonth = "";
+
 for (var blogPost of blogJson) {
 	// Container
 	var post = document.createElement("div");
@@ -8,7 +10,7 @@ for (var blogPost of blogJson) {
 	authorImage.classList.add("blog_image");
 	authorImage.src = "./resources/team/" + blogPost.author.toLowerCase() + ".png";
 	post.appendChild(authorImage);
-
+	
 	// Text
 	post.appendChild(getElement("blog_author", blogPost.author));
 	post.appendChild(getElement("blog_date", blogPost.date));
@@ -17,6 +19,26 @@ for (var blogPost of blogJson) {
 	// Add to page
 	var blog = document.getElementById("blog")
 	blog.insertBefore(post, blog.firstChild);
+	
+	// Add navigation button
+	var month = blogPost.date.substr(3, 2);
+	if(month != latestNavMonth){
+		var blogNav = document.getElementById("blog_navigation");
+		
+		var button = document.createElement("button");
+		button.classList.add("blog_button");
+		button.innerHTML = getMonthString(month) + " " + blogPost.date.substr(6, 4);
+
+		var anchorId = getMonthString(month) + "_anchor";
+		post.id = anchorId;
+		button.onclick = (function () {
+			  var anchorElement = document.getElementById(anchorId);
+			  return function () { anchorElement.scrollIntoView(); }
+		})();
+
+		blogNav.insertBefore(button, blogNav.firstChild);
+		latestNavMonth = month;
+	}
 }
 
 function getElement(className, text){
@@ -24,4 +46,33 @@ function getElement(className, text){
 	element.classList.add(className);
 	element.innerHTML = text;
 	return element;
+}
+
+function getMonthString(month){
+	switch(month){
+		case "01":
+			return "January";
+		case "02":
+			return "Feb";
+		case "03":
+			return "March";
+		case "04":
+			return "April";
+		case "05":
+			return "May";
+		case "06":
+			return "June";
+		case "07":
+			return "July";
+		case "08":
+			return "August";
+		case "09":
+			return "September";
+		case "10":
+			return "October";
+		case "11":
+			return "November";
+		case "12":
+			return "December";
+	}
 }
